f089f01408c6a685844fdc7710a63c117e8b9d88
Nilesh Ram <47472286+nileshram@users.noreply.github.com>
2022-07-18 17:47:50 +0100

    update to strategies factory

--- src/strategy/leg.py
+++ src/strategy/leg.py
@@ -2,10 +2,13 @@
 
 class Leg:
 
-    def __init__(self):
-        pass
+    def __init__(self, params=None):
+        self._init_params(params)
 
-    def _init_params(self):
-        pass
+    def _init_params(self, params=None):
+        #store the fixed strike option details in each leg
+        self.exp_date = params.exdate
+        self.strike = params.strike_price
+        self.opt_type = params.cp_flag
 
     
--- src/strategy/strategy.py
+++ src/strategy/strategy.py
@@ -3,6 +3,7 @@ from configuration import ConfigurationFactory
 from datetime import datetime
 from manager.data import DataManager
 import pandas as pd
+from strategy.leg import Leg
 from utility.date_utility import DateUtility
 
 class StrategyFactory:
@@ -13,7 +14,7 @@ class StrategyFactory:
         #load the configuration but purely for dates
         self._init_date_config()
         #step 1: initialise the filtered set of trading dates based on leg entry frequency
-        self.create_strategy_trading_dates()
+        self.create_strategies()
         #step 2: for each of the trading dates, we need to trade the correct set of expiries
 
     def _init_params(self):
@@ -56,7 +57,7 @@ class StrategyFactory:
         _final_trading_dates = pd_trading_dates[_mask].to_list()
         return _final_trading_dates
 
-    def create_strategy_trading_dates(self):
+    def create_strategies(self):
         """
         We generate the list of trading dates based on the entry frequency
         of each leg using pandas date range
@@ -66,7 +67,7 @@ class StrategyFactory:
 
         :return: A list of trading dates based on the trading parameters
         """
-
+        print("{} - Initialising strategies".format(datetime.now()))
         #load frequencies and expiries from config
         self._leg_freq = self._config["backtest_config"]["trading_params"]["entry_freq"]
         self._opt_expiry_calendar = self._config["backtest_config"]["trading_params"]["option_expiry_calendar"]
@@ -105,43 +106,60 @@ class StrategyFactory:
                 #step 1: first get all of the new weeklys that we trade on this date
                 #i.e. if we dont have any new weeklys to trade on this date then we skip this from the data
                 if (data[data["date"] == trade_date]["dte"] == new_leg_mty).value_counts().index[0] is True:
-                    #then we construct the strategy here
-                    _tmp = data[data["date"] == trade_date].copy()
-                    #we now filter for the delta strikes
-                    _tmp
+                    print("{} - Creating strategies for trade date: {}".format(datetime.now(),
+                                                                               trade_date))
                     self.strategies[year][trade_date] = Strategy(config_params=self._multileg_config,
                                                                  trade_date=trade_date,
                                                                  data=data)
-                #iterate through each of the rows in the trading date and we need to check
+                    print("{} - Finished creating strategies for trade date: {}".format(datetime.now(),
+                                                                                        trade_date))
+                else:
+                    print("{} - Skipping trade date: {} as there is no leg data".format(datetime.now(),
+                                                                                        trade_date))
 
-                #if the delta strike is in the bucket and filter for these strikes
-
-
-
-            print("stop")
-
-
-
-
-    def create_strategies(self):
-        print("{} - Initialising strategies".format(datetime.now()))
 
 class Strategy:
 
     def __init__(self, config_params=None, data=None, trade_date=None):
+        #initialise config
+        self._init_config(config_params)
+        #create the data filter for the trade date
+        self._tmp = data[data["date"] == trade_date]
+        #apply delta strike range
+        self._apply_delta_strike_filter()
 
-        #filter the data fpr legs
-        _filtered_data = data[data["date"] == trade_date]
+        #initialise call and put legs
+        self._init_call_legs(data=data)
+        self._init_put_legs(data=data)
 
+    def _init_config(self, config_params):
+        self._multileg_config = config_params
 
-        self._init_config()
-        self._parse_dates()
+    def _apply_delta_strike_filter(self):
+        self._call_delta_strikes = self._tmp.loc[(self._tmp['bs_delta'] >= self._multileg_config["call_legs"]["delta_strike_range"][1]) \
+                                                 & (self._tmp['bs_delta'] <= self._multileg_config["call_legs"]["delta_strike_range"][0])]
+        self._put_delta_strikes = self._tmp.loc[(self._tmp['bs_delta'] >= self._multileg_config["put_legs"]["delta_strike_range"][1]) \
+                                                 & (self._tmp['bs_delta'] <= self._multileg_config["put_legs"]["delta_strike_range"][0])]
 
-    def _init_config(self):
-        pass
+    def _init_call_legs(self, data=None):
+        self.call_legs = {}
+        for idx in range(len(self._call_delta_strikes)):
+            leg_id = "leg_{}".format(str(idx + 1))
+            _leg = Leg(params=self._call_delta_strikes.iloc[idx])
+            _leg.leg_data = data.loc[(data["exdate"] == _leg.exp_date) & \
+                                     (data["strike_price"] == _leg.strike) & \
+                                     (data["cp_flag"] == _leg.opt_type)]
+            #add to the leg collection
+            self.call_legs[leg_id] = _leg
 
-    def _parse_dates(self):
-        pass
+    def _init_put_legs(self, data=None):
+        self.put_legs = {}
+        for idx in range(len(self._put_delta_strikes)):
+            leg_id = "leg_{}".format(str(idx + 1))
+            _leg = Leg(params=self._put_delta_strikes.iloc[idx])
+            _leg.leg_data = data.loc[(data["exdate"] == _leg.exp_date) & \
+                                     (data["strike_price"] == _leg.strike) & \
+                                     (data["cp_flag"] == _leg.opt_type)]
+            #add to the leg collection
+            self.put_legs[leg_id] = _leg
 
-    def _init_legs(self):
-        pass
